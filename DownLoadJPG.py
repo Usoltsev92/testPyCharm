a4d3eceb690f067bf55fb0309741c884d96d4595
Usoltsev Alexander <Usoltsev92@icloud.com>
2023-07-13 23:04:33 +0700

    download jpg module lesson21

--- DownLoadJPG.py
+++ DownLoadJPG.py
@@ -1,10 +1,12 @@
 import random
 import urllib.request
-
+names = []
 def download_img(url):
     name = random.randrange(1,100)
     name = str(name) + ".jpg"
     urllib.request.urlretrieve(url, name)
+    names.append(name)
 
 
 download_img("https://w.forfun.com/fetch/71/71cf5a68d51acab5f06c69c96b81c8a4.jpeg")
+print(names)
